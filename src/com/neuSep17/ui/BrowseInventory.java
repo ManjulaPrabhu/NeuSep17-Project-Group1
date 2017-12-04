8002bc0b958ce12ab71271912bd559cf6ac08b60
ManjulaPrabhu <31738763+ManjulaPrabhu@users.noreply.github.com>
2017-12-04 13:45:10 -0800

    Updated Group1 Team1 WorkItems

---
+++ src/com/neuSep17/ui/BrowseInventory.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class BrowseInventory {
+    public static void main(String[] args) throws IOException {
+        BrowseInventoryScreen inventoryScreen =new BrowseInventoryScreen();
+        inventoryScreen.setComponents();
+        inventoryScreen.addListeners();
+
+    }
+}
